517a479aec6e11c7f6b4baac8d97956f75606017
Drew Lee <drewhlee75@gmail.com>
2020-03-27 19:26:08 -0400

    Student-model-created

--- proj_2/settings.py
+++ proj_2/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pullupclub.apps.PullupclubConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- pullupclub/models.py
+++ pullupclub/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField()
+    age = models.IntegerField()
+    pullUpCount = models.IntegerField()
+    record_date = models.DateTimeField()
+    slug = models.SlugField()
