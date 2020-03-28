f8c1955e165cad8463ac953dd1cc7c74e60581b1
Drew Lee <drewhlee75@gmail.com>
2020-03-28 00:29:02 -0400

    Fixed-Student-Model

--- pullupclub/models.py
+++ pullupclub/models.py
@@ -3,9 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    name = models.CharField(max_length=100, blank=True, null=True)
-    email = models.EmailField()
-    age = models.IntegerField()
-    pullUpCount = models.IntegerField()
-    record_date = models.DateTimeField()
-    slug = models.SlugField()
+    name = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=10)
+    slug = models.SlugField(unique=True)
