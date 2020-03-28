f63e107ae68c45c4db91645a9ec485aa7882a0e5
Drew Lee <drewhlee75@gmail.com>
2020-03-28 01:05:25 -0400

    Added-Function-to-Show-Name-of-Records-On-Admin-Panel

--- pullupclub/models.py
+++ pullupclub/models.py
@@ -3,11 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    name = models.CharField(max_length=100)
-    nickname = models.CharField(max_length=10)
+    name = models.CharField(max_length=100, default="Sam Doe")
+    nickname = models.CharField(max_length=10, default="Anonymous")
     slug = models.SlugField(unique=True)
-
+    def __str__(self):
+        return self.name
 
 class PullUpSession(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     amount_of_pullups = models.IntegerField()
+    def __str__(self):
+        return ("No. " + str(self.pk))
