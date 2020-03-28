8511c1c1f77a3139ea1c3e446b07feac87dd6717
Drew Lee <drewhlee75@gmail.com>
2020-03-28 00:50:01 -0400

    PullUpSession-Model-Created

--- pullupclub/models.py
+++ pullupclub/models.py
@@ -6,3 +6,8 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     nickname = models.CharField(max_length=10)
     slug = models.SlugField(unique=True)
+
+
+class PullUpSession(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    amount_of_pullups = models.IntegerField()
