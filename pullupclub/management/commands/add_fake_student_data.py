3c3c452abdb809628a887f526a104482e7a9834b
Jon_Stricoff <jon@helloalma.com>
2020-03-29 14:27:50 -0400

    added commands

---
+++ pullupclub/management/commands/add_fake_student_data.py
@@ -0,0 +1,14 @@
+
+from django.core.management.base import BaseCommand
+
+import random
+
+""" add fake students and pullup sessions
+"""
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        pass
+
+
+
---
+++ pullupclub/management/commands/add_fake_student_data_reset.py
@@ -0,0 +1,15 @@
+
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+from pullupclub.models import Student
+
+""" Delete all student data then re-add fake students and pullup sessions
+"""
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        count = Student.objects.delete()
+        print("students deleted", count)
+
+        call_command("add_fake_student_data")
