ad6d1644da0917673f0abf439b53ef76fe0b682f
Drew Lee <drewhlee75@gmail.com>
2020-03-29 16:32:32 -0400

    Added-Awards-Model

--- pullupclub/admin.py
+++ pullupclub/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Student, PullUpSession
+from .models import Student, PullUpSession, Award
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(PullUpSession)
+admin.site.register(Award)
 
